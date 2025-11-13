b4d5f2daef83a485991fd511ab09da50f2f17f8a
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-11-13 18:45:15 +0200

    Revert Header position

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,19 +1,14 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Topbar from "../components/topbar";
 import Header from "../components/Header";
-import ContactSection from "../components/ContactSection";
 import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 z-50">
-        <Topbar />
-        <Header />
-      </div>
-
+      <Topbar />
+      <Header />
       <Outlet />
-      
       <Footer />
     </>
   );
