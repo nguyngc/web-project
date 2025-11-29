2176882ed1252cca2261153ab70fffa849c4b791
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-11-29 18:49:55 +0200

    Update Services Page/ Service Details Page to load data dynamic from db

--- frontend/src/components/ServiceDetail.jsx
+++ frontend/src/components/ServiceDetail.jsx
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom";
-import dataServices from "../data/dataServices";
 import { Link } from "react-router-dom";
-import { Calendar, Clock, DollarSign} from "lucide-react";
+import { Calendar, Clock, DollarSign } from "lucide-react";
+import Spinner from "./common/Spinner";
+import useFetch from "../hooks/useFetch";
 
 function ServiceDetail() {
-    const { slug } = useParams();
-    const service = dataServices.find((s) => s.url.endsWith(slug));
+    const { id } = useParams();
+    const { data: service, loading, error } = useFetch(`/api/services/${id}`, {}, true);
 
-    if (!service) {
+    if (loading) {
+        return <Spinner loading={loading} />
+    }
+
+    if (!service || error) {
         return (
             <div className="p-8">
                 <h2 className="text-red-600 text-xl">Service not found</h2>
@@ -18,14 +23,18 @@ function ServiceDetail() {
         );
     }
 
+    const { serviceName: name, image, shortDescription, fullDescription, duration, price, frequency, features, benefits } = service;
+    console.log(service);
+    const featureList = features.split(';');
+    const benefitList = benefits.split(';');
 
     return (
         <div className="flex flex-col">
             {/* Hero section */}
             <section className="relative h-[300px] md:h-[400px] w-full">
                 <img
-                    src={service.image}
-                    alt={service.name}
+                    src={image}
+                    alt={name}
                     className="w-full h-full object-cover"
                 />
 
@@ -42,10 +51,10 @@ function ServiceDetail() {
                 {/* Overlay */}
                 <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-[1040px] bg-white flex flex-col justify-center py-6 rounded-t-lg px-6 md:px-10">
                     <h1 className="text-[#2b3eac] text-2xl md:text-4xl font-medium">
-                        {service.name}
+                        {name}
                     </h1>
                     <p className="text-[#2b3eac] text-base md:text-xl font-medium mt-4">
-                        {service.shortDescription}
+                        {shortDescription}
                     </p>
                 </div>
             </section>
@@ -59,39 +68,39 @@ function ServiceDetail() {
                             <Clock className="w-6 h-6 text-white" />
                         </div>
                         <h3 className="font-semibold">Duration</h3>
-                        <p>{service.duration}</p>
+                        <p>{duration}</p>
                     </div>
                     <div className="bg-[#FEE9E9] rounded-lg p-6 text-center flex flex-col items-center gap-2">
                         <div className="flex justify-center items-center w-12 h-12 rounded-full bg-[#159EEC]">
                             <DollarSign className="w-6 h-6 text-white" />
                         </div>
                         <h3 className="font-semibold">Price</h3>
-                        <p>{service.price}</p>
+                        <p>{price}</p>
                     </div>
                     <div className="bg-[#FEE9E9] rounded-lg p-6 text-center flex flex-col items-center gap-2">
                         <div className="flex justify-center items-center w-12 h-12 rounded-full bg-[#159EEC]">
                             <Calendar className="w-6 h-6 text-white" />
                         </div>
                         <h3 className="font-semibold">Frequency</h3>
-                        <p>{service.frequency}</p>
+                        <p>{frequency}</p>
                     </div>
                 </div>
 
                 {/* Full description */}
                 <div className="bg-[#FEE9E9] shadow-md rounded-lg p-6 mb-12">
                     <p className="font-inter text-[16px] leading-[24px] text-black whitespace-pre-line">
-                        {service.fullDescription}
+                        {fullDescription}
                     </p>
                 </div>
 
                 {/* What's Included & Key Benefits */}
-                <div className="grid md:grid-cols-2 gap-6 mb-20">
+                <div className="grid md:grid-cols-2 gap-6">
                     <div className="rounded-lg border border-pink-200 p-6">
                         <h3 className="font-inter text-[16px] leading-[24px] text-black font-semibold mb-4">
                             What's Included
                         </h3>
                         <ul className="list-disc pl-6 space-y-2">
-                            {service.features.map((f, i) => (
+                            {featureList.map((f, i) => (
                                 <li key={i}>{f}</li>
                             ))}
                         </ul>
@@ -102,7 +111,7 @@ function ServiceDetail() {
                             Key Benefits
                         </h3>
                         <ul className="list-disc pl-6 space-y-2">
-                            {service.benefits.map((b, i) => (
+                            {benefitList.map((b, i) => (
                                 <li key={i}>{b}</li>
                             ))}
                         </ul>
